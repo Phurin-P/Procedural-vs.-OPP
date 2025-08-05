6a5327d728a001dca1c8ad59b85f260b0b33cbda
Phurin-P <sirinaraparn_p@silpakorn.edu>
2025-08-05 11:41:19 +0700

    Procedural.py

--- Procedural.py
+++ Procedural.py
@@ -1,4 +1,4 @@
-product_list = []
+product_list = {}
 
 def add_product(product_list, product_name, product_quantity):
     if product_name in product_list:
@@ -13,4 +13,5 @@ def show_products(product_list):
 add_product(product_list, "Snack", 10)
 add_product(product_list, "Drink", 20)
 add_product(product_list, "Snack", 10)
+
 show_products(product_list)
